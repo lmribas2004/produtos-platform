be6953d55ada378ce0146531ce4f607e4fbc0961
lmribas2004 <lmribas2004@gmail.com>
2026-03-04 16:24:20 -0300

    TEste de pipeline

--- backend/app/Http/Controllers/Api/ProdutoController.php
+++ backend/app/Http/Controllers/Api/ProdutoController.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Exception;
 
 /*
-Controlador de API para gerenciamento de produtos.
+Controlador de API para gerenciamento de produtos REST.
 */
 class ProdutoController extends Controller
 {
