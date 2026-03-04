0ea9406ebfd2e8f338718f5927adfb0b7a37480e
lmribas2004 <lmribas2004@gmail.com>
2026-03-04 16:47:16 -0300

    Melhoria pipeline

--- backend/app/Http/Controllers/Api/ProdutoController.php
+++ backend/app/Http/Controllers/Api/ProdutoController.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Exception;
 
 /*
-Controlador de API para gerenciamento de produtos REST.
+Controlador de API para gerenciamento de produtos.
 */
 class ProdutoController extends Controller
 {
